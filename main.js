7d925f029b1456a3c148f57f3e9cabc5e8bb8190
Ilya Skryabin <808lorem@gmail.com>
2017-11-17 16:29:08 +0300

    radio

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ function fun1() {
     var result = (chbox.checked) ? 'On' : 'Off';
     console.log(result);
 }
+
+function fun2() {
+    var radio = document.getElementsByName('r1');
+    for(var i = 0; i < radio.length; i++) {
+        if(radio[i].checked) {
+            var result = "On " + radio[i].value;
+            return console.log(result);
+        }
+    }
+    console.log('Nothing');
+}
