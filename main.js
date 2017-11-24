3dd492fe42407b6b1b67879d484edc18d80db97b
Ilya Skryabin <808lorem@gmail.com>
2017-11-25 00:05:03 +0300

    To do app

---
+++ main.js
@@ -0,0 +1,54 @@
+var myNodeList = document.getElementsByTagName('LI'),
+	i;
+
+for(i = 0; i < myNodeList.length; i++) {
+    var span = document.createElement("SPAN"),
+		txt = document.createTextNode("\u00D7");
+    span.className = 'close';
+    span.appendChild(txt);
+    myNodeList[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName('close'),
+	j;
+
+for(j = 0; j < close.length; j++) {
+    close[j].onclick = function () {
+		var div = this.parentElement;
+		div.style.display = 'none';
+	}
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+	if(ev.target.tagName == "LI") {
+		ev.target.classList.toggle('checked');
+	}
+}, false);
+
+function newElement() {
+	var li = document.createElement('li'),
+		inputValue = document.getElementById('myInput').value,
+		t = document.createTextNode(inputValue);
+	li.appendChild(t);
+	if(inputValue === '') {
+	    alert('Введите тектс');
+	} else {
+		document.getElementById('myUl').appendChild(li);
+	}
+
+	document.getElementById('myInput').value = "";
+
+	var span = document.createElement("SPAN"),
+		txt = document.createTextNode("\u00D7");
+	span.className = 'close';
+	span.appendChild(txt);
+	li.appendChild(span);
+
+	for(var j = 0; j < close.length; j++) {
+		close[j].onclick = function () {
+			var div = this.parentElement;
+			div.style.display = 'none';
+		}
+	}
+}
