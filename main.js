f2708cb8090f6da6da8a8b733ab2713af2c1d379
Ilya Skryabin <808lorem@gmail.com>
2017-11-27 16:51:07 +0300

    alert

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 function myFunction() {
 	var x = document.getElementById('myDiv');
-	x.style.display = (x.style.display == 'none') ? '' : 'none';
+	x.style.display = (x.style.display === 'none') ? '' : 'none';
+}
+
+var close = document.getElementsByClassName('closebtn'),
+	i;
+
+for(i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+		var div = this.parentElement;
+		div.style.opacity = '0';
+		setTimeout(function () {
+			div.remove();
+		}, 600)
+	}
 }
